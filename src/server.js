31f404f2621e1411f80ce6d2dc900935219b0f7c
Sheetal <dsheetal555@gmail.com>
2022-06-21 23:36:36 +0530

    sixth commit

--- src/server.js
+++ src/server.js
@@ -17,6 +17,3 @@ app.get('/*', function(req, res) {
     res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
   );
   });
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
